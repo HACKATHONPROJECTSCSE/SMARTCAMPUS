b58ee4d396cf9ce856d4e2aa0cf640e4300b2279
SAURABHJADHAV.CSE <saurabhjadhav.cse@gmail.com>
2023-03-16 18:25:58 +0530

    FIRST COMMIT

---
+++ app/src/main/java/com/saurabhjadhav/smartcampus/Authentication/Login.java
@@ -0,0 +1,16 @@
+package com.saurabhjadhav.smartcampus.Authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.saurabhjadhav.smartcampus.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
