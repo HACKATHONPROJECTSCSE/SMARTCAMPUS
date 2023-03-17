9bd4a15c5e3197cdbcd20645950c057a90f5f202
omkokate <om.kokate2902@gmial.com>
2023-03-17 13:08:57 +0530

    calling module

--- app/src/main/java/com/saurabhjadhav/smartcampus/Student/ImportantContacts/ComputerStaffContacts.java
+++ app/src/main/java/com/saurabhjadhav/smartcampus/Student/ImportantContacts/ComputerStaffContacts.java
@@ -3,32 +3,91 @@ package com.saurabhjadhav.smartcampus.Student.ImportantContacts;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.saurabhjadhav.smartcampus.R;
+import com.saurabhjadhav.smartcampus.Student.Attendance.PA;
+import com.saurabhjadhav.smartcampus.Student.Attendance.PAAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ComputerStaffContacts extends AppCompatActivity {
 
-    Button oakSir;
+//    Button oakSir;
 
+    TextView name_faculty,faculty_number;
+
+    RecyclerView recyclerViewContacts;
+    List<Contact> ContactList;
+    ContactAdapter ContactAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_staff_contacts);
 
-        oakSir = findViewById(R.id.oakSir);
+        recyclerViewContacts = findViewById(R.id.recyclerViewContacts);
 
-        oakSir.setOnClickListener(new View.OnClickListener() {
+
+        ContactList = new ArrayList<>();
+
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference csRef = db.collection("cs");
+        csRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
-            public void onClick(View view) {
-                Intent callIntent1 = new Intent(Intent.ACTION_DIAL);
-                callIntent1.setData(Uri.parse("tel:" + "123456789"));
-                startActivity(callIntent1);
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    // Iterate through each document
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        String name = document.getString("name");
+                        String number = document.getString("number");
+
+                        Log.d("myapp",""+name);
+                        Log.d("myapp",""+number);
+
+                        Contact ct = new Contact(name, number);
+                        ContactList.add(ct);
+
+
+
+                    }
+                    ContactAdapter.notifyDataSetChanged();
+                    recyclerViewContacts.setAdapter(ContactAdapter);
+
+
+                } else {
+                    Log.d("myapp", "Error getting documents: ", task.getException());
+                }
+
+
+
             }
         });
+
+
+        recyclerViewContacts.setHasFixedSize(true);
+        recyclerViewContacts.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        ContactAdapter = new ContactAdapter(getApplicationContext(), ContactList);
 
     }
 }
