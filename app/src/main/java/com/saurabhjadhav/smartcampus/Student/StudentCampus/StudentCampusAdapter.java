f0348b1c07969e9fbd1df715c269a0dcf002eb6f
omkokate <om.kokate2902@gmial.com>
2023-03-17 22:33:54 +0530

    campus and hostel image download

--- app/src/main/java/com/saurabhjadhav/smartcampus/Student/StudentCampus/StudentCampusAdapter.java
+++ app/src/main/java/com/saurabhjadhav/smartcampus/Student/StudentCampus/StudentCampusAdapter.java
@@ -1,11 +1,15 @@
 package com.saurabhjadhav.smartcampus.Student.StudentCampus;
 
+import android.app.DownloadManager;
 import android.content.Context;
+import android.net.Uri;
+import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -39,6 +43,27 @@ public class StudentCampusAdapter extends RecyclerView.Adapter<StudentCampusAdap
         StudentCampusModel studentCampusModel = mList.get(position);
         holder.campusDescription.setText(studentCampusModel.getCampusdescriptionUrl());
 
+        holder.campusDownloader.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    String imageUrl = mList.get(holder.getAdapterPosition()).getCampusimageUrl();
+                    String fileName = "campus.jpg";
+
+                    DownloadManager downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+                    DownloadManager.Request request = new DownloadManager.Request(Uri.parse(imageUrl))
+                            .setTitle(fileName)
+                            .setDescription("Downloading image")
+                            .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+                            .setAllowedOverMetered(true)
+                            .setAllowedOverRoaming(true)
+                            .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName);
+
+                    downloadManager.enqueue(request);
+
+                    Toast.makeText(context, "Image downloading...", Toast.LENGTH_SHORT).show();
+                }
+        });
+
     }
 
     @Override
@@ -49,13 +74,14 @@ public class StudentCampusAdapter extends RecyclerView.Adapter<StudentCampusAdap
     public static class MyViewHolder extends RecyclerView.ViewHolder {
 
         public TextView campusDescription;
-        ImageView campusImages;
+        ImageView campusImages,campusDownloader;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
 
             campusImages = itemView.findViewById(R.id.CampusImageLoader);
             campusDescription = itemView.findViewById(R.id.CampusDescriptionLoader);
+            campusDownloader=itemView.findViewById(R.id.CampusImageDownloader);
 
         }
     }
--- app/src/main/java/com/saurabhjadhav/smartcampus/Student/StudentCampus/StudentCampusHostelAdapter.java
+++ app/src/main/java/com/saurabhjadhav/smartcampus/Student/StudentCampus/StudentCampusHostelAdapter.java
@@ -1,11 +1,17 @@
 package com.saurabhjadhav.smartcampus.Student.StudentCampus;
 
+import android.app.DownloadManager;
 import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -40,7 +46,29 @@ public class StudentCampusHostelAdapter extends RecyclerView.Adapter<StudentCamp
 
         StudentCampusModel studentCampusModel = mList.get(position);
         holder.hostelDescription.setText(studentCampusModel.getHosteldescriptionUrl());
+
+        holder.hostelDownload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String imageUrl = mList.get(holder.getAdapterPosition()).getHostelimageUrl();
+                String fileName = "hostel.jpg";
+
+                DownloadManager downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(imageUrl))
+                        .setTitle(fileName)
+                        .setDescription("Downloading image")
+                        .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+                        .setAllowedOverMetered(true)
+                        .setAllowedOverRoaming(true)
+                        .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName);
+
+                downloadManager.enqueue(request);
+
+                Toast.makeText(context, "Image downloading...", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 
     @Override
     public int getItemCount() {
@@ -50,13 +78,15 @@ public class StudentCampusHostelAdapter extends RecyclerView.Adapter<StudentCamp
     public static class MyViewHolder extends RecyclerView.ViewHolder {
 
         public TextView hostelDescription;
-        ImageView hostelImages;
+        ImageView hostelImages, hostelDownload;
+
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
 
             hostelImages = itemView.findViewById(R.id.HostelImageLoader);
             hostelDescription = itemView.findViewById(R.id.HostelDescriptionLoader);
+            hostelDownload= itemView.findViewById(R.id.HostelImageDownloader);
 
         }
     }
