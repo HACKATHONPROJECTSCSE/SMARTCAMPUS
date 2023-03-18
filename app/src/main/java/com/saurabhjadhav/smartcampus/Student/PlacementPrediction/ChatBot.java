f213c4193286bd2156dba4c129400cbf01f1b64f
SAURABHJADHAV.CSE <saurabhjadhav.cse@gmail.com>
2023-03-18 06:39:58 +0530

    FINAL COMMIT

--- app/src/main/java/com/saurabhjadhav/smartcampus/Student/PlacementPrediction/ChatBot.java
+++ app/src/main/java/com/saurabhjadhav/smartcampus/Student/PlacementPrediction/ChatBot.java
@@ -1,9 +1,10 @@
 package com.saurabhjadhav.smartcampus.Student.PlacementPrediction;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.saurabhjadhav.smartcampus.R;
 
@@ -14,10 +15,10 @@ public class ChatBot extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_bot);
 
-
-
-
         WebView webView = findViewById(R.id.webview);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl("https://friday-mu.vercel.app/");
 
     }
